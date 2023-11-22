6676286d6507f691c31faa837ec3756cf517246f
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2023-11-22 13:51:20 +0700

    corrected arabictoroman.cpp

--- arabtoroman.cpp
+++ arabtoroman.cpp
@@ -9,17 +9,17 @@
 
 using namespace std;
 
-string ArabToRoman(int num) {
-  int Arab[13] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+string ArabicToRoman(int num) {
+  int Arabic[13] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
   string Roman[13] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
   int index = 12;
   string res = "";
   
   while (num > 0) {
-    while (Arab[index] > num) { 
+    while (Arabic[index] > num) { 
       --index; 
     }
-    num -= Arab[index];
+    num -= Arabic[index];
     res += Roman[index];
   }
   return res;
@@ -28,7 +28,7 @@ string ArabToRoman(int num) {
 int main() {
   int number;
   cin >> number;
-  cout << "Римское число: " << ArabToRoman(number);
+  cout << "Римское число: " << ArabicToRoman(number);
   cout << "Арабское число: " << number << endl;
   return 0;
 }
